fb62ea6540e1a602f881e4a154517574615ef82c
TechMulan <ceovertical@gmail.com>
2025-02-13 13:29:54 +0100

    Commit

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-// // Simple form validation
-// document.getElementById('contact-form').addEventListener('submit', function (e) {
-//     e.preventDefault();
+// Simple form validation
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
     
-//     const name = document.getElementById('name').value;
-//     const email = document.getElementById('email').value;
-//     const message = document.getElementById('message').value;
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
     
-//     if (name && email && message) {
-//       alert('Thank you for reaching out, ' + name + '! I will get back to you soon.');
-//       document.getElementById('contact-form').reset(); // Reset form after submission
-//     } else {
-//       alert('Please fill in all fields.');
-//     }
-//   });
+    if (name && email && message) {
+      alert('Thank you for reaching out, ' + name + '! I will get back to you soon.');
+      document.getElementById('contact-form').reset(); // Reset form after submission
+    } else {
+      alert('Please fill in all fields.');
+    }
+  });
   
